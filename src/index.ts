302072f05491810fbe16bb7edd21ad61b0785fdb
TingSong-Syu <rek@rek.me>
2022-04-21 18:48:56 +0800

    export types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { T } from './Rules/TypeRuleChains';
 export { R } from './Rules/EmptyRule';
 export { ValidateError } from './ValidateError';
 export { default } from './validate';
+export type { Rules, Chainable, Validator } from './Chain';
